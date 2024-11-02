6cb0afd89aac142692015829907e1c0a3ac52d45
Muja Kayadan <mujakayadan@outlook.com>
2024-11-02 11:19:45 -0500

    Book.java

--- src/main/java/org/example/citylibrary/model/Book.java
+++ src/main/java/org/example/citylibrary/model/Book.java
@@ -1,4 +1,14 @@
 package org.example.citylibrary.model;
 
+import jakarta.persistence.*;
+
+@Entity(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "book_id")
+    private int bookId;
+    private String title;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Publisher publisher; // Book -X-------||- Publisher
 }
--- src/main/java/org/example/citylibrary/model/Publisher.java
+++ src/main/java/org/example/citylibrary/model/Publisher.java
@@ -1,9 +1,11 @@
 package org.example.citylibrary.model;
-
+import org.example.citylibrary.model.Book;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -14,11 +16,16 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer publisherId;
+
     @Column(name = "publisher_name")
     private String publisherName;
+
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "address_id")
     private Address primaryAddress;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Book> books;
 
     public Publisher(String publisherName) {
         this.publisherName = publisherName;
